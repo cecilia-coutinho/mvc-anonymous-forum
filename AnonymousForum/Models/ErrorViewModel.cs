5042b913e7151e251dcf4348ad9d569d2796b4e3
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-08-30 17:01:10 +0200

    environment setup

---
+++ AnonymousForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnonymousForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
