1f6b2e7b85a8e468b6c93f0295e141cc1f501cf0
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-09-02 14:15:34 +0200

    TopicModel atributes added

--- AnonymousForum/Models/Topic.cs
+++ AnonymousForum/Models/Topic.cs
@@ -1,6 +1,16 @@
-﻿namespace AnonymousForum.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnonymousForum.Models
 {
     public class Topic
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Invalid name or Format")]
+        [StringLength(30, MinimumLength = 3), DisplayName("Topic Name"), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string? TopicName { get; set; }
     }
 }
