0da8fdb4b990dbf032b94316f8d3a6c1bcba8cdb
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-09-02 14:48:17 +0200

    validation update

--- AnonymousForum/Models/Topic.cs
+++ AnonymousForum/Models/Topic.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace AnonymousForum.Models
 {
@@ -13,5 +14,8 @@ namespace AnonymousForum.Models
         [Required(ErrorMessage = "Invalid name or Format")]
         [StringLength(30, MinimumLength = 3), DisplayName("Topic Name"), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? TopicName { get; set; }
+
+        [JsonIgnore]
+        public List<Thread>? Threads { get; set; }
     }
 }
