9b3cdba8c7ee0f3b4e9909ffae42009ea1be2eb8
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-09-02 14:54:55 +0200

    ReplyModel attributes added

--- AnonymousForum/Models/Reply.cs
+++ AnonymousForum/Models/Reply.cs
@@ -1,6 +1,26 @@
-﻿namespace AnonymousForum.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace AnonymousForum.Models
 {
     public class Reply
     {
+
+        [Key]
+        public int ReplyId { get; set; }
+
+        [StringLength(40, MinimumLength = 5), DisplayName("Title")]
+        public string? ReplyTitle { get; set; }
+
+        [Required(ErrorMessage = "Please insert a text")]
+        [StringLength(40, MinimumLength = 5), DisplayName("Description")]
+        public string? ReplyDescription { get; set; }
+
+        [Required]
+        public int FkTopicId { get; set; }
+
+        [JsonIgnore]
+        public virtual Topic? Topics { get; set; }
     }
 }
