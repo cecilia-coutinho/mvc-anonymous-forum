1d3325227bb005cf41302001dae2f3bdbc3bb2d4
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-09-02 14:49:59 +0200

    validation update

--- AnonymousForum/Models/Thread.cs
+++ AnonymousForum/Models/Thread.cs
@@ -1,10 +1,26 @@
-﻿namespace AnonymousForum.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AnonymousForum.Models
 {
     public class Thread
     {
+        [Key]
         public int ThreadId { get; set; }
+
+        [Required(ErrorMessage = "Please insert a valid title")]
+        [StringLength(40, MinimumLength = 5), DisplayName("Title")]
         public string? ThreadTitle { get; set; }
+
+        [Required(ErrorMessage = "Please insert a description")]
+        [DisplayName("Description")]
         public string? ThreadDescription { get; set;}
+
+        [Required]
         public int FkTopicId { get; set; }
+
+        [JsonIgnore]
+        public virtual Topic Topics { get; set; }
     }
 }
